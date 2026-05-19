0ae41f8c69ba8cea7797f05baf626ed465230341
@amiin key <146594005+Amiin-key@users.noreply.github.com>
2026-05-19 12:31:33 +0300

    Refactor fetch event handling in service worker

--- sw.js
+++ sw.js
@@ -2,27 +2,22 @@ const cacheName = 'ssm-invest-v1';
 const staticAssets = [
   './',
   './index.html',
-  './ssm.js',
   './manifest.json'
 ];
 
-// Instalka Service Worker-ka
 self.addEventListener('install', async el => {
   const cache = await caches.open(cacheName);
   await cache.addAll(staticAssets);
   return self.skipWaiting();
 });
 
-// Firfircoonida iyo nadiifinta cash-ka hore
 self.addEventListener('activate', el => {
   self.clients.claim();
 });
 
-// Soo jiidashada xogta (Offline Support)
 self.addEventListener('fetch', async el => {
   const req = el.request;
   const url = new URL(req.url);
-
   if (url.origin === location.origin) {
     el.respondWith(cacheFirst(req));
   } else {
@@ -43,7 +38,6 @@ async function networkAndCache(req) {
     await cache.put(req, refresh.clone());
     return refresh;
   } catch (hi) {
-    const cached = await cache.match(req);
-    return cached;
+    return await cache.match(req);
   }
 }
